c5267cf5c1cc50ba48aa924651205e4eb588142f
ashwindevx <ashwindevx@gmail.com>
2023-01-12 13:00:21 +0530

    setup tailwind + added assets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-primary">
+      <h1 className="text-3xl font-galano font-normal">Hello world!</h1>
+      <h1 className="text-3xl font-galano font-medium">Hello world!</h1>
+      <h1 className="text-3xl font-galano font-semibold">Hello world!</h1>
+      <img src="../public/images/white.svg" alt="" />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#146EB4",
+        secondary: "#FAFAFA",
+        dark: "#1A181E",
+        medium: "#4D4D4D",
+        light: "#F2F2F2",
+      },
+      fontFamily: {
+        galano: ["Galano Grotesque", "Segoe UI"],
+      },
+    },
+  },
+  plugins: [],
+};
